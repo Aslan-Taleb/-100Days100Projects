a1587d0c33a53eb20a8117ba4c2d9ed928147a97
AslaN <aslantalebselim@gmail.com>
2023-01-20 13:44:02 +0100

    Project

--- 17.Hirst_painting/library.py
+++ 17.Hirst_painting/library.py
@@ -1,7 +1,20 @@
 from turtle import *
+import colorgram as c
 
 
 def myScreen():
     screen = Screen()
-    screen.screensize(800, 400)
+    screen.colormode(255)
+    screen.screensize(1000, 1000)
     screen.exitonclick()
+
+
+def color_generate(image):
+    colors = c.extract(image, 35)
+    myColors = []
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        myColors.append((r, g, b))
+    return myColors
--- 17.Hirst_painting/main.py
+++ 17.Hirst_painting/main.py
@@ -1,13 +1,28 @@
+
+import random
 from library import *
 
 
 def main():
-    t = Turtle()
-    t.shape("turtle")
-    t.color("blue")
-    t.setpos(0, 0)
-    num_sides = 3
+    timmy = Turtle()
+    colormode(255)
+    timmy.pensize(20)
+    myColors = color_generate("image.jpg")
+    go = 50
+    timmy.penup()
+    timmy.setx(-400)
+    timmy.sety(-350)
+    pos_y = -350
+
     while True:
+        for _ in range(10):
+            pos_x, _ = timmy.position()
+            timmy.pendown()
+            timmy.dot(random.choice(myColors))
+            timmy.penup()
+            timmy.goto(pos_x+go, pos_y)
+        timmy.setx(-400)
+        pos_y += 50
 
     myScreen()
 
