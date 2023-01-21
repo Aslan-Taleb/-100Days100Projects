0dbd90005f7349d5081543bec3691d484d17b041
AslaN <aslantalebselim@gmail.com>
2023-01-21 09:16:29 +0100

    Projet

--- 17.Hirst_painting/library.py
+++ 17.Hirst_painting/library.py
@@ -2,13 +2,6 @@ from turtle import *
 import colorgram as c
 
 
-def myScreen():
-    screen = Screen()
-    screen.colormode(255)
-    screen.screensize(1000, 1000)
-    screen.exitonclick()
-
-
 def color_generate(image):
     colors = c.extract(image, 35)
     myColors = []
--- 17.Hirst_painting/main.py
+++ 17.Hirst_painting/main.py
@@ -4,27 +4,33 @@ from library import *
 
 
 def main():
-    timmy = Turtle()
-    colormode(255)
-    timmy.pensize(20)
     myColors = color_generate("image.jpg")
-    go = 50
+    colormode(255)
+
+    screen = Screen()
+    screen.bgcolor("white")
+    screen.setup(width=600, height=600)
+
+    timmy = Turtle()
+    timmy.hideturtle()
     timmy.penup()
-    timmy.setx(-400)
-    timmy.sety(-350)
-    pos_y = -350
+    timmy.setx(-250)
+    timmy.sety(-250)
+    timmy.speed("fastest")
 
-    while True:
+    step = 50
+
+    for _ in range(10):
         for _ in range(10):
-            pos_x, _ = timmy.position()
+            pos_x, pos_y = timmy.position()
             timmy.pendown()
-            timmy.dot(random.choice(myColors))
+            timmy.dot(20, random.choice(myColors))
             timmy.penup()
-            timmy.goto(pos_x+go, pos_y)
-        timmy.setx(-400)
-        pos_y += 50
+            timmy.goto(pos_x+step, pos_y)
+        timmy.setx(-250)
+        timmy.sety(pos_y+50)
 
-    myScreen()
+    screen.exitonclick()
 
 
 main()
