a244fb5e2fa7c677ceb064037999912bdc2807ea
AslaN <aslantalebselim@gmail.com>
2023-01-23 10:33:44 +0100

    Project

---
+++ DAY 1_band-name-generator/main.py
@@ -0,0 +1,13 @@
+#greeting for the program.
+print("Hello ! ")
+print("Welcome to the Band Name Generator.")
+#Ask city that they grew up in.
+city = input("-What's name of the city you grew up in ?\n")
+#Ask name of a pet.
+pet = input("-What's your pet's name ?\n")
+#Combine the name of their city and pet and show them their band name.
+band = '"'+city + " " + pet +'"'
+print("Your band name could be "+band)
+print("pretty cool huh ?")
+
+#Made by AslaN
