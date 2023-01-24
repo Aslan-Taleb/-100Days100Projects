b2df286dbcb0ac3c4415bafa210586f772f4ee43
AslaN <aslantalebselim@gmail.com>
2023-01-24 23:40:23 +0100

    Project

---
+++ 24.States-Game/library.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+data = pandas.read_csv('50_states.csv')
+
+
+def check_answer(answer_state):
+    if not data[data['state'] == answer_state].empty:
+        x = int(data[data['state'] == answer_state]['x'])
+        y = int(data[data['state'] == answer_state]['y'])
+        return x, y
+    else:
+        return None, None
+
+
+def the_turtle_writer(x, y, answer_state):
+    timmy = turtle.Turtle()
+    timmy.hideturtle()
+    timmy.penup()
+    timmy.goto(x, y)
+    timmy.write(answer_state, align='center', font=("Arial", 10, "normal"))
+
+
+def learn(guessed_states):
+    state_to_learn = []
+    message = "Hey,you haven't found a 'few' states, I'll put you the list to learn what you have left : \n"
+    all_states = data['state'].to_list()
+    for state in all_states:
+        if state not in guessed_states:
+            state_to_learn.append(state)
+    for i in state_to_learn:
+        message += i + "\n"
+    message += "Good Luck ! "
+    print(message)
+
+
+def say_exit_cheat():
+    timmy = turtle.Turtle()
+    timmy.hideturtle()
+    timmy.penup()
+    timmy.goto(250, -300)
+    timmy.write("Type 'exit' to Exit.\nType 'cheat' to cheat.", align='center',
+                font=("Arial", 10, "normal"))
--- 24.States-Game/main.py
+++ 24.States-Game/main.py
@@ -1,9 +1,47 @@
-# with open("weather_data.csv", mode="r") as file:
-#     data = []
-#     myData = file.readlines()
 
-#     data.append()
-# print(data)
-import cvs
-with open("weather_data.csv", mode="r") as file:
-    data = cvs.reader(file)
+from library import *
+import time
+from art import *
+NUMBER_STATE = 50
+screen = turtle.Screen()
+screen.title("STATES GAME")
+image = "USA.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+game_is_on = True
+x = None
+y = None
+
+guessed_states = []
+answer_to_avoid_bug_cancel = ""
+answer_state = ""
+say_exit_cheat()
+while game_is_on:
+    if len(guessed_states) < 50:
+        screen.bgcolor("white")
+        answer_to_avoid_bug_cancel = screen.textinput(
+            title=f"{len(guessed_states)}/{NUMBER_STATE} States Correct", prompt="What's another state's ?")
+        if answer_to_avoid_bug_cancel is not None:
+            answer_state = answer_to_avoid_bug_cancel.title()
+        x, y = check_answer(answer_state)
+        if x and y is not None:
+            screen.bgcolor("green")
+            the_turtle_writer(x, y, answer_state)
+            if answer_state not in guessed_states:
+                guessed_states.append(answer_state)
+        else:
+            screen.bgcolor("red")
+            time.sleep(0.4)
+        x = None
+        y = None
+    else:
+        print("\t\tYou Won ! ")
+        print(victory)
+        game_is_on = False
+    if answer_state == "Exit":
+        break
+    if answer_state == "Cheat":
+        print(cheat)
+if game_is_on:
+    learn(guessed_states)
