8475fbae844601b59d1ba23bcf7ad616dc97f499
AslaN <aslantalebselim@gmail.com>
2023-01-24 11:06:46 +0100

    Project

--- 23.Mail-Merge/library.py
+++ 23.Mail-Merge/library.py
@@ -1,3 +1,6 @@
+REPLACE = '[name]'
+
+
 def create_letter():
     number_letter = 0
     with open("Input/Letters/starting_letter.txt", mode="r") as myLetter_file:
@@ -7,7 +10,7 @@ def create_letter():
             names_str = names_str.replace(" ", "")
             names_list = names_str.split()
             for name in names_list:
-                myLetter_str_new = myLetter_str.replace("[name]", name)
+                myLetter_str_new = myLetter_str.replace(REPLACE, name)
                 with open(f"./Output/ReadyToSend_{name}.txt", mode="w") as readyToSend:
                     readyToSend.write(myLetter_str_new)
                     print(f"Letter Created For : {name}\n")
